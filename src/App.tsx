a6cee61dd5e523ebf6751ed2c0e88e482f5c3707
Roman Kostiv <kostiv.rr2005@gmail.com>
2026-02-01 10:44:17 +0200

    feat: set up new project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import MainLayout from "./templates/main-layout/main-layout.tsx";
+
+const App = () => {
+
+    return (
+        <>
+            <MainLayout />
+        </>
+    );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/styles/_variables.scss" as v;
+          @use "@/styles/_mixins.scss" as m;
+        `,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+})
